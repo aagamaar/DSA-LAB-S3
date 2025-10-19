dd6fd2d7ec8c62f9e3181b6ea2256db17a0e0d32
aagamaar <aagamaar01@gmail.com>
2025-10-19 21:36:39 +0530

    added fourth c program i.e. void function with no parameters

---
+++ C_BASICS/prgm4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void sayHello() //Void function does not return any value
+{ 
+    printf("Hello\n");
+}
+
+int main()
+{
+    sayHello(); //Function call
+    return 0;
+}

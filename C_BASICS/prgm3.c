8a591164e4e121d6ec902d75045533ec95bde005
aagamaar <aagamaar01@gmail.com>
2025-10-19 19:11:43 +0530

    added third program to store numbers in array, printing them and calculating their sum

---
+++ C_BASICS/prgm3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*int number[5] = {1,2,3,4,5};
+    printf("%d\n",number[0]);
+    printf("%d\n",number[1]);
+    printf("%d\n",number[2]);
+    printf("%d\n",number[3]);
+    printf("%d\n",number[4]);
+    return 0;*/
+
+    int num_arr[5];
+    printf("Enter 5 numbers to store in the array:\n");
+    for (int i=0; i<5; i++){
+        scanf("%d",&num_arr[i]);
+    }
+    
+    printf("The numbers stored in the array are:\n");
+    for (int i=0; i<=4; i++){
+        printf("%d\t",num_arr[i]);
+    }
+
+    printf("\nThe total sum of elelment in the array is:\n");
+    int sum = 0;
+    for (int i = 0; i<=4; i++){
+        sum+= num_arr[i];
+        return sum;
+    }
+
+    return 0;
+}

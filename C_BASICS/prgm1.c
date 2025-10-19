3643467abc8055e5ca6ca7d3d711d26153196b24
aagamaar <aagamaar01@gmail.com>
2025-10-19 17:00:06 +0530

    added placeholders for printing different data types in first c program

--- C_BASICS/prgm1.c
+++ C_BASICS/prgm1.c
@@ -4,6 +4,10 @@
 int main()
 {
     printf("Hello World\n");
+    printf("%d\n",12);
+    printf("%f\n",2.45);
+    printf("%c\n",'a');
+    printf("%s","Aagama");
     return 0;// The program will terminate here, so the next line is unreachable.
     printf("This line will never be executed.\n");
     /*This is a 

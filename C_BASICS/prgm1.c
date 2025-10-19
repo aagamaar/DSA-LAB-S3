79327c8f7275274a6f058a2cf6100a1b74f5e6f4
aagamaar <aagamaar01@gmail.com>
2025-10-19 17:44:34 +0530

    used scanf for taking inputs of different data types in first c program

--- C_BASICS/prgm1.c
+++ C_BASICS/prgm1.c
@@ -3,11 +3,24 @@
 
 int main()
 {
-    printf("Hello World\n");
-    printf("%d\n",12);
-    printf("%f\n",2.45);
-    printf("%c\n",'a');
-    printf("%s","Aagama");
+    int a;
+    double b;
+    char name[15];
+    char d;
+    
+    printf("Enter the integer value a :\n");
+    scanf("%d",&a);
+    printf("Enter the double value b :\n");
+    scanf("%lf",&b);
+    printf("Enter your name : \n");
+    scanf("%s",name);
+    printf("Enter the character 'd' :\n");
+    scanf(" %c",&d);
+    printf("*******************************************\n");
+    printf("%d\n",a);
+    printf("%f\n",b);
+    printf("%c\n",d);
+    printf("%s",name);
     return 0;// The program will terminate here, so the next line is unreachable.
     printf("This line will never be executed.\n");
     /*This is a 
